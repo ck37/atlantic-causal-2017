2fc69c695e9adae4a4820d3c7548b69bb439c4ff
Jonathan Levy <jl@jlstiles.com>
2017-04-18 22:30:45 -0700

    small changes, had to remember we will predict for Q0W and update it only...

--- fluctuate.R
+++ fluctuate.R
@@ -1,13 +1,23 @@
+# test on basic data
 W = rnorm(1000)
 A = rbinom(1000,1,plogis(W+.4))
 Y = rnorm(1000,.1*A+.2*W+1,2)
 Y = (Y-min(Y))/(max(Y)-min(Y))
-Q0Wfit = glm(Y~A+W,family='binomial')
+QAWfit = glm(Y~A+W,family='binomial')
 gfit = glm(A~W,family='binomial')
-Q0W = predict(Q0Wfit,type='response')
+
+# set up to predict Q0W
+X = data.frame(W=W,A=A)
+X0 = X
+X0$A = 0
+
+# predict Q0W and g
+Q0W = predict(Q0Wfit,newdata=X0,type='response')
 g = predict(gfit, type='response')
 
+# put in data.frame or list
 initdata = data.frame(A=A,Y=Y,Q0W=Q0W,g=g)
+
 # takes initdata, dataframe or list containing elements, Q0W,Y,g,A 
 update <- function(initdata) {
   H = with(initdata, (A==0)*g/(mean(A)*(1-g)))
