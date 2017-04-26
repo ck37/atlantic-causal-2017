eb2e398f11b2d8c36b48eb2069fe020a8789c374
Chris Kennedy <chrisken@gmail.com>
2017-04-26 12:51:54 -0700

    Add multicore to test-2016; check unit-level estimates

--- test-2016.R
+++ test-2016.R
@@ -1,22 +1,32 @@
 # Dependencies: import-2016.R
 source("lib/function_library.R")
 
-# Set auto-install to T for code to install any missing packages.
-load_all_packages(auto_install = F,
-                  verbose = T)
-
-# Load all .R files in the lib directory.
-ck37r::load_all_code("lib", verbose = T)
-
 # Assumes 2016 data is extracted to inbound/data-2016/
 # Download from https://drive.google.com/file/d/0B8TUkApaUlsGekFSblJWa25NM1E/edit
 conf = list(
   inbound_dir = "inbound/data-2016",
+
   # Subdirectory to save temporary csv files.
   data_dir = "data",
+
   # Subdirectory to save output logs.
-  output_dir = "output"
+  output_dir = "output",
+
+  # Set to T for extra output during execution.
+  verbose = T,
+
+  # Set auto-install to T for code to install any missing packages.
+  auto_install = F,
+
+  # Use up to this many cores if available.
+  max_cores = 4
 )
+
+# Set auto-install to T for code to install any missing packages.
+load_all_packages(auto_install = conf$auto_install, verbose = conf$verbose)
+
+# Load all .R files in the lib directory.
+ck37r::load_all_code("lib", verbose = conf$verbose)
 
 input_file = paste0(conf$data_dir, "/import-2016.RData")
 
@@ -31,6 +41,21 @@ names(files[[1]])
 
 data = files[[1]]$data
 
+# Setup parallelization? Use up to 4 cores.
+num_cores = RhpcBLASctl::get_num_cores()
+
+if (conf$verbose) {
+  cat("Cores detected:", num_cores, "\n")
+}
+
+use_cores = min(num_cores, conf$max_cores)
+options("mc.cores" = use_cores)
+
+if (conf$verbose) {
+  # Check how many parallel workers we are using:
+  cat("Cores used:", getOption("mc.cores"), "\n")
+}
+
 q_lib = c("SL.mean", "SL.glmnet")
 g_lib = c("SL.mean", "SL.glmnet")
 
@@ -42,6 +67,13 @@ results = estimate_att(A = data$z,
                        W = data_x,
                        SL.library = q_lib,
                        g.SL.library = g_lib,
-                       verbose = T)
+                       verbose = conf$verbose)
 
+# Extract out unit estimates to make displaying more convenient.
+unit_estimates = results$unit_estimates
+results$unit_estimates = NULL
+
+# Display everything except the unit estimates.
 results
+
+summary(unit_estimates)
