bbb9223e2c7f9690be2fd68907525bcefd7e13fe
Chris Kennedy <chrisken@gmail.com>
2017-04-29 17:45:37 -0700

    Annotate simulation, add safety checks, tweak code style

--- fluctuate.R
+++ fluctuate.R
@@ -6,21 +6,38 @@ truncate <- function(x, lower = 0.01, upper = 1 - lower) {
 
 # takes initdata, dataframe or list containing elements, Q0W,Y,g,A
 update <- function(initdata) {
-  H = with(initdata, (A==0)*g/(mean(A)*(1-g)))
 
-  # fit a glm with the weight
+  # Create clever covariate, which is 0 for treatment units.
+  H = with(initdata, (A == 0) * g / (mean(A) * (1 - g)))
+
+  # Fit a glm putting the clever covariate in the weight.
   fit = glm(Y ~ offset(qlogis(Q0W)),
             weights = H, data = initdata,
             family = "binomial")
 
-  eps = fit$coefficients[1]
+  # Extract epsilon from the MLE.
+  epsilon_hat = fit$coefficients[1]
 
-  # update
-  Q0Wstar = with(initdata, plogis(qlogis(Q0W) + eps))
+  if (epsilon_hat == 0) {
+    warning("Epsilon hat estimated to be zero.")
+  }
+
+  # Update estimated Q(0, W) - we only care about the treated units though.
+  Q0Wstar = with(initdata, plogis(qlogis(Q0W) + epsilon_hat))
+
+  # Calculate percentage of treated units that changed after fluctuation update.
+  pct_changed = mean(initdata$Q0W[initdata$A == 1] != Q0Wstar[initdata$A == 1])
+
+  # If the treated units didn't change there is some sort of issue.
+  if (pct_changed == 0) {
+    warning("Fluctuation did not change any potential outcomes for treated units.")
+  }
 
   # Compile results.
   results = list(Q0Wstar = Q0Wstar,
-                 epsilon = eps)
+                 pct_changed = pct_changed,
+                 epsilon_hat = epsilon_hat)
+
   return(results)
 }
 
--- fluctuate_simulation.R
+++ fluctuate_simulation.R
@@ -8,8 +8,9 @@ Q0=function(A,W1,W2,W3,W4) return(A+2*A*W4+3*W1+1*W2^2+.5*W3*W4+.25*W4)
 Q0=function(A,W1,W2,W3,W4) return(15*A*(W4<-1)+1*A*(W3^2<1)+.63*W1+1*W2^2+A*.5*cos(W3)+.1*W4*W1+A*(W2>1))
 g0=function(W1,W2,W3,W4) {plogis(-.28*W1+5*W2^2*W4+.08*W3+5*abs(W4)-1)}
 g0=function(W1,W2,W3,W4) {plogis(-.28*W1+5*W2 + W4+.08*W3 -1)}
+
 # random draw of sample size n--continuous unscaled Y
-gendata=function(n){
+gendata = function(n) {
   U1 = runif(n,0,1)
   W1= -1*(U1<=.5)+(U1>.5)
   W2=rnorm(n)
@@ -43,20 +44,26 @@ sim_fluctuate = function(n) {
   X = data
   X$Y = NULL
 
-  # fits
+  # Estimate outcome under observed treatment status.
   QAWfit = suppressWarnings(glm(Y ~ ., data = data, family = 'binomial'))
+
+  # Estimate propensity score.
   gfit = glm(A ~ ., data = X, family = 'binomial')
 
-  # predict Q0W and g
+  # Predict Q0W and g
   X0 = data
   X0$A = 0
-  Q0W = suppressWarnings(predict(QAWfit , newdata = X0, type = 'response'))
-  g = predict(gfit, type = 'response')
 
-  # put in data.frame or list
-  initdata = data.frame(Q0W = Q0W, Y = data$Y, g = g, A = data$A)
+  # Predict smoothed potential outcome (Q_bar) under A = control.
+  Q0W = suppressWarnings(predict(QAWfit, newdata = X0, type = 'response'))
 
-  # fluctuate by logistic regression
+  # Predict propensity score.
+  g_hat = predict(gfit, type = 'response')
+
+  # Compile data needed for fluctuation.
+  initdata = data.frame(Q0W = Q0W, Y = data$Y, g = g_hat, A = data$A)
+
+  # Fluctuate by logistic regression
   update_results = suppressWarnings(update(initdata))
   Q0Wstar = update_results$Q0Wstar
 
@@ -75,16 +82,25 @@ sim_fluctuate = function(n) {
 }
 
 # run B sims of n=1000 compile results
-B=200
-n=1000
-res=mclapply(1:B,FUN = function(x) sim_fluctuate(1000),mc.cores=4)
-res=t(sapply(res,FUN=function(x) x))
-colnames(res)=c("true","logistic")
+B = 200
+n = 1000
+res = mclapply(1:B, FUN = function(x) sim_fluctuate(1000), mc.cores = 4)
+
+if (F) {
+  # Non-parallel version for running manually if desired.
+  # This may show additional output compared to the parallel version.
+  res = lapply(1:B, FUN = function(x) sim_fluctuate(1000))
+}
+
+res = t(sapply(res, FUN = function(x) x))
+colnames(res) = c("true", "logistic")
 
 # we can see if there's a difference, should be very little
-df=data.frame(type=c(rep("true",B),rep("logistic",B)),est=c(res[,1],res[,2]))
-bins=50
-e = (max(df$est)-min(df$est))/bins
+df = data.frame(type = c(rep("true", B), rep("logistic", B)),
+              est = c(res[,1], res[,2]))
+bins = 50
+e = (max(df$est) - min(df$est)) / bins
 
-gghist = ggplot(df,aes(x=est,fill=type))+geom_density(alpha=.3)
+gghist = ggplot(df,aes(x = est, fill = type)) +
+  geom_density(alpha = .3) + theme_minimal()
 gghist
