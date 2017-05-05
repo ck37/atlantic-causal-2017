f680a64c53659a31d3bc4d1d4ca3a296e0c8e089
Jonathan Levy <jl@jlstiles.com>
2017-05-05 14:34:53 -0700

    slight fix

---
+++ simulations/lastyear_explore.R
@@ -0,0 +1,37 @@
+# exploration from last year
+
+# look at treatment,  Y and Y given A=1, Y given  A=0
+
+# getting names in matrix to call in and store y and z values 
+
+addon = rbind(c(0,0,0),c(0,0,1),c(0,1,0),c(0,1,1),c(1,0,0),c(1,0,1),c(1,1,0),c(1,1,1))
+addon
+dataset_names = do.call(cbind,lapply(1:4,FUN = function(i) {
+  vapply(1:8, FUN=function(nm) {
+    y=paste0("inbound/pre_data/",paste0(i,"."),paste(addon[nm,],collapse=""),".y.csv")
+    z=paste0("inbound/pre_data/",paste0(i,"."),paste(addon[nm,],collapse=""),".z.csv")
+    return(c(z,y))
+  },FUN.VALUE = c("adsf","sdf"))
+}))
+
+# list of dataframes per run
+AYlist = lapply(1:32,FUN=function(x) {
+  A=read.csv(dataset_names[1,x])
+  Y=read.csv(dataset_names[2,x])
+  return(data.frame(A=A,Y=Y))
+})
+
+# get basic info
+basics = data.frame(t(sapply(AYlist, FUN = function(x) {
+  Abar = mean(x$z)
+  Ybar = mean(x$y)
+  Ysd = sd(x$y)/sqrt(length(x$y))
+  Ygiven1 = sum(with(x,z*y))/sum(x$z)
+  Ygiven0 = sum(with(x,(1-z)*y))/sum(1-x$z)
+  ATE = Ygiven1-Ygiven0
+  return(c(Abar,Ybar,Ysd,Ygiven1,Ygiven0,ATE))
+})))
+
+colnames(basics) = c("Abar","Ybar","Ysd","Ygiven1","Ygiven0","ATE")
+
+basics
