a7ca82163693be3b18cdf99fe2b3ed27cf541c01
Chris Kennedy <chrisken@gmail.com>
2017-05-08 16:46:18 -0700

    Tweak default pre-screening setup

--- lib/estimate_att.R
+++ lib/estimate_att.R
@@ -26,7 +26,7 @@ estimate_att =
            verbose = F,
            # Added optional prescreening: (0,0) if you don't want it
            # First term is p-value cut-off, second is minimum # of terms.
-           prescreen = c(0.05, 10),
+           prescreen = c(0.20, 12),
            # If T use SL estimation, otherwise use GLM.
            useSL = T
            ) {
